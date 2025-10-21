b29a23241d79e2a7a348328ee146d9daab1ac124
khushiy05 <khushiyewale150@gmail.com>
2025-10-21 21:10:18 +0530

    files

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+const rootElement = document.getElementById("root");
+createRoot(document.getElementById('root')).render(
+    <App />
+)
