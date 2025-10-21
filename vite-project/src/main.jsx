9448a9abe54ade7ef40f4638df9caf69581e8475
khushiy05 <khushiyewale150@gmail.com>
2025-10-21 21:20:05 +0530

    main file

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,4 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-const rootElement = document.getElementById("root");
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const rootElement = createRoot(document.getElementById("root"));
+rootElement.render(<>Hi</>);
